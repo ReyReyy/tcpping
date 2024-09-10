98acc9a471ab21b8f28b26815cdec74199f66aaf
ReyReyy <iam@reyreyy.net>
2024-09-10 22:49:41 +0800

    Add --upgrade.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
+use reqwest::blocking::get;
+use semver::Version;
+use serde_json::Value;
 use std::env;
+use std::fs;
 use std::net::{IpAddr, TcpStream, ToSocketAddrs};
-use std::process;
+use std::path::Path;
+use std::process::{self, Command};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::{Duration, Instant};
+use tempfile::NamedTempFile;
+
+fn get_latest_version() -> Option<(String, bool)> {
+    let url = "https://api.github.com/repos/ReyReyy/tcpping/releases/";
+    match get(url) {
+        Ok(response) => {
+            let json: Value = response.json().ok()?;
+            let releases = json.as_array()?;
+            let latest_release = releases.first()?;
+            let latest_version = latest_release["tag_name"]
+                .as_str()?
+                .trim_start_matches('v')
+                .to_string();
+            let is_prerelease = latest_release["prerelease"].as_bool().unwrap_or(false);
+            Some((latest_version, is_prerelease))
+        }
+        Err(_) => None,
+    }
+}
+
+fn is_installed() -> bool {
+    Path::new("/usr/local/bin/tcpping").exists()
+}
+
+fn install() -> Result<(), std::io::Error> {
+    let current_exe = env::current_exe()?;
+    fs::copy(current_exe, "/usr/local/bin/tcpping")?;
+    Ok(())
+}
 
 fn tcp_ping(
     program_name: &str,
@@ -137,21 +171,96 @@ fn tcp_ping(
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let program_name = args[0].clone(); // 使用完整的程序路径
+    let program_name = args[0].clone();
 
     match args.get(1).map(|s| s.as_str()) {
         Some("-v") | Some("--version") => {
-            let version = env!("CARGO_PKG_VERSION");
+            let current_version = env!("CARGO_PKG_VERSION");
             let arch = std::env::consts::ARCH;
             let os = std::env::consts::OS;
-            println!("tcpping version {} ({}/{})", version, os, arch);
+            println!("tcpping version {} ({}/{})", current_version, os, arch);
+
+            if let Some((latest_version, is_prerelease)) = get_latest_version() {
+                let current_is_prerelease = current_version.contains("-");
+                if Version::parse(&latest_version).unwrap()
+                    > Version::parse(current_version).unwrap()
+                    || (current_is_prerelease && !is_prerelease)
+                {
+                    println!(
+                        "New version available: v{}. run 'tcpping --upgrade' to update",
+                        latest_version
+                    );
+                }
+            }
             process::exit(0);
         }
         Some("-h") | Some("--help") => {
             eprintln!(
-                "Usage: {} <host> [--port <port>] [--IPv4 | --IPv6] [--count <count>]",
+                "Usage: {} <host> [--port <port>] [--IPv4 | --IPv6] [--count <count>] [--upgrade]",
                 program_name
             );
+            process::exit(0);
+        }
+        Some("--upgrade") => {
+            if !is_installed() {
+                eprintln!(
+                    "tcpping not installed. Please run '{} --install' first.",
+                    program_name
+                );
+                process::exit(1);
+            }
+
+            let current_version = env!("CARGO_PKG_VERSION");
+            if let Some((latest_version, is_prerelease)) = get_latest_version() {
+                let current_is_prerelease = current_version.contains("-");
+                if Version::parse(&latest_version).unwrap()
+                    <= Version::parse(current_version).unwrap()
+                    && !(current_is_prerelease && !is_prerelease)
+                {
+                    println!("This version is already the latest version.");
+                    process::exit(0);
+                }
+            }
+
+            println!("Upgrading tcpping...");
+
+            // Create a temporary file
+            let temp_file = NamedTempFile::new().expect("Failed to create temporary file");
+            let temp_path = temp_file.path().to_str().unwrap().to_string();
+
+            // Download the install script
+            let response =
+                get("https://raw.githubusercontent.com/ReyReyy/tcpping/master/shell/install.sh")
+                    .expect("Faild to download install script");
+            let content = response.text().expect("Failed to read install script");
+
+            // Write the script content to the temporary file
+            fs::write(&temp_path, content).expect("Failed to write to temporary file");
+
+            // Run the install script with the --upgrade parameter
+            let status = Command::new("sudo")
+                .arg("bash")
+                .arg(&temp_path)
+                .arg("--upgrade")
+                .status()
+                .expect("Failed to execute install script");
+
+            process::exit(status.code().unwrap_or(1));
+        }
+        Some("--install") => {
+            if is_installed() {
+                eprintln!("tcpping already installed. No need to install again.");
+                process::exit(1);
+            }
+
+            println!("Installing tcpping...");
+            match install() {
+                Ok(_) => println!("tcpping has been installed successfully"),
+                Err(e) => {
+                    eprintln!("Installation failed: {}", e);
+                    process::exit(1);
+                }
+            }
             process::exit(0);
         }
         _ => {}
